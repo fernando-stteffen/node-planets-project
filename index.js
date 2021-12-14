8cdc75ba57a5f1e2ff16c261974c0a26092310c0
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-12-14 08:48:39 -0300

    feat: add limiter of self-size relation with earth filter to list of kapler

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const habitablePlanets = []
 
 const isHabitablePlanet = (planet) => planet['koi_disposition'] === 'CONFIRMED'
   && planet['koi_insol'] > 0.33 && planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6
 
 fs.createReadStream('kapler_data_12_21.csv')
   .pipe(parse({
